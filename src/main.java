58ccb44b3d0f82bf8b473a90135f82b36af99894
JohnDohnut <51821505+JohnDohnut@users.noreply.github.com>
2021-09-26 17:15:25 +0900

    Add files via upload

---
+++ src/main.java
@@ -0,0 +1,13 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                System.out.printf("곱셈수 : %d\n", i);
+                System.out.printf("피곱셈수 : %d\n", j);
+                System.out.printf("결과 : %d\n",i*j);
+                System.out.printf("\n");
+            }
+
+        }
+    }
+}
